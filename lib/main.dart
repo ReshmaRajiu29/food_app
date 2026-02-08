47b56687334b26827f3c3cde4eab572fcf884beb
Reshma Raji <reshmarajiu290501@gmail.com>
2026-02-08 22:02:36 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'pages/root.dart';
+import 'theme/color.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Food App',
+      theme: ThemeData(
+        primaryColor: primary,
+        textTheme: GoogleFonts.latoTextTheme(
+          Theme.of(context).textTheme,
+        ),
+      ),
+      home: const RootApp(),
+    );
+  }
+}
